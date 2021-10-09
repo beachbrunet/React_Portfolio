407fae1f41c443be40c9e0a2391e6106c1ce8c75
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-10-09 16:17:23 -0600

    added home page

---
+++ src/componets/MainPage/home.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const home = () => {};
+
+export default home;
--- src/componets/Nav/index.js
+++ src/componets/Nav/index.js
@@ -4,21 +4,15 @@ import { Nav } from "react-bootstrap";
 const Nav = () => {
   <Nav fill variant="tabs" defaultActiveKey="/home">
     <Nav.Item>
-      <Nav.Link href="/home">Active</Nav.Link>
+      <Nav.Link href="/home">Home</Nav.Link>
     </Nav.Item>
 
     <Nav.Item>
-      <Nav.Link eventKey="link-1">Loooonger NavLink</Nav.Link>
+      <Nav.Link eventKey="link-1">Something</Nav.Link>
     </Nav.Item>
 
     <Nav.Item>
-      <Nav.Link eventKey="https://github.com/beachbrunet">Github</Nav.Link>
-    </Nav.Item>
-
-    <Nav.Item>
-      <Nav.Link eventKey="disabled" disabled>
-        Disabled
-      </Nav.Link>
+      {/* <Nav.Link eventKey="https://github.com/beachbrunet">Github</Nav.Link> */}
     </Nav.Item>
   </Nav>;
 };
