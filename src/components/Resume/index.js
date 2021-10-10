3a3ac59b91f11bac3f844337dccacee0627b751c
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-10-10 10:12:14 -0600

    saving resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,13 +3,27 @@ import Resume from "../../assets/olderResume.pdf";
 
 const resume = () => {
   return (
-    <div class="mt-5">
+    <div class="container">
       <h2 className="top-title">Resume</h2>
       <ul>
-        <li>Full-Stack Web Developer</li>
+        <li>Full-Stack Web Developer and my Proficiencies</li>
+        <h6>
+          <ul>
+            <li>Html5</li>
+            <li>Css</li>
+            <li>Javascript</li>
+            <li>JQuery</li>
+            <li>Bootstrap</li>
+            <li>Node.js</li>
+            <li>MySQL, MongoDb, Mongoose</li>
+            <li>Express</li>
+            <li>React</li>
+            <li>HandleBars</li>
+          </ul>
+        </h6>
       </ul>
       <h1>
-        <img src={Resume} alt="resume" />
+        <img src={Resume} alt=" " />
       </h1>
     </div>
   );
