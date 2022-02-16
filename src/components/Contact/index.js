c88ed36027c9d322a42d6d1e1b8bb3e0579b76bd
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2022-02-16 16:54:46 -0700

    editing contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,27 +1,33 @@
 import React from "react";
 import "./contact.css";
 // import { validateEmail } from "../../utils/helpers";
-// currently no back end to make
+// currently no back end to make;
 
+// To prevent submiting empty feilds
 const contactPage = () => {
+  function App() {
+    const onSubmit = (event) => {
+      event.preventDefault();
+      console.log("submission prevented");
+    };
+
+    return (
+      <form onSubmit={onSubmit}>
+        <input />
+        <input type="submit" />
+      </form>
+    );
+  }
+  // Need a function to email when on submit
+  // dont forget to do a correct email layout as well
   return (
     <div class="container">
       <form action="/action_page.php">
         <label for="fname">First Name</label>
-        <input
-          type="text"
-          id="fname"
-          name="firstname"
-          placeholder="Your name.."
-        ></input>
+        <input type="text" id="fname" name="firstname"></input>
 
         <label for="lname">Last Name</label>
-        <input
-          type="text"
-          id="lname"
-          name="lastname"
-          placeholder="Your last name.."
-        ></input>
+        <input type="text" id="lname" name="lastname"></input>
 
         <label for="country">Country</label>
         <select id="country" name="country">
@@ -30,11 +36,11 @@ const contactPage = () => {
           <option value="usa">USA</option>
         </select>
 
-        <label for="subject">Subject</label>
+        <label for="subject">Message</label>
         <textarea
           id="subject"
           name="subject"
-          placeholder="Write something.."
+          placeholder="Your Message"
         ></textarea>
 
         <input type="submit" value="Submit"></input>
@@ -42,4 +48,5 @@ const contactPage = () => {
     </div>
   );
 };
+
 export default contactPage;
