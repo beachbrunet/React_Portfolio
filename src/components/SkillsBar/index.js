7b5ec8b2678fe801f2c4f702690ed71ebbfce9c0
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2023-01-18 14:00:36 -0700

    fixed background color and cleaning up navbar

--- src/components/SkillsBar/index.js
+++ src/components/SkillsBar/index.js
@@ -1,28 +1,28 @@
-import React from "react";
-import SkillBar from "react-skillbars";
+// import React from "react";
+// import SkillBar from "react-skillbars";
 
-// Dont forget to adjust the type of skills
-const doSomething = function () {
-  [
-    { type: "Java", level: 100 },
-    { type: "React", level: 85 },
-    { type: "Javascript", level: 75 },
-    { type: "Spring", level: 50 },
-    { type: "Docker", level: 25 },
-    { type: "HTML", level: 20 },
-    { type: "NoSQL", level: 0 },
-  ];
-};
-export default doSomething;
-
-// const colors = {
-//   bar: {
-//     hue: 0,
-//     saturation: 50,
-//     level: {
-//       minimum: 80,
-//       maximum: 100,
-//     },
-//   },
+// // Dont forget to adjust the type of skills
+// const doSomething = function () {
+//   [
+//     { type: "Java", level: 100 },
+//     { type: "React", level: 85 },
+//     { type: "Javascript", level: 75 },
+//     { type: "Spring", level: 50 },
+//     { type: "Docker", level: 25 },
+//     { type: "HTML", level: 20 },
+//     { type: "NoSQL", level: 0 },
+//   ];
 // };
-// <SkillBar skills={skills} colors={colors} />;
+// export default doSomething;
+
+// // const colors = {
+// //   bar: {
+// //     hue: 0,
+// //     saturation: 50,
+// //     level: {
+// //       minimum: 80,
+// //       maximum: 100,
+// //     },
+// //   },
+// // };
+// // <SkillBar skills={skills} colors={colors} />;
