3c3025585f8430200e6cce1fe6c73a691af5837c
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2023-01-19 15:47:50 -0700

    cleaned up Nav bar, need to add a documents tab

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,38 +1,3 @@
-// import { Nav } from "react-bootstrap";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// const Navbar = () => {
-//   return (
-//     <Nav fill variant="tabs" defaultActiveKey="/home">
-//       <Nav.Item>
-//         <Nav.Link href="/">Home</Nav.Link>
-//       </Nav.Item>
-//       {/* LinkedIn*/}
-//       <Nav.Item>
-//         <Nav.Link
-//           href="https://www.linkedin.com/in/catherine-cox-433061128/"
-//           target="_blank"
-//         >
-//           LinkedIn
-//         </Nav.Link>
-//       </Nav.Item>
-//       {/* Github  */}
-//       <Nav.Item>
-//         <Nav.Link href="https://github.com/beachbrunet" target="_blank">
-//           Github
-//         </Nav.Link>
-//       </Nav.Item>
-//       {/* Contact */}
-//       <Nav.Item>
-//         <Nav.Link href="/contact">Contact</Nav.Link>
-//       </Nav.Item>
-//       {/* Resume */}
-//       <Nav.Item>
-//         <Nav.Link href="/resume">Resume</Nav.Link>
-//       </Nav.Item>
-//     </Nav>
-//   );
-// };
-
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
@@ -45,8 +10,17 @@ function darkNav() {
         <Container>
           <Navbar.Brand href="#home"> {"<> "} Home</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#Github"> Github </Nav.Link>
-            <Nav.Link href="#LinkedIn"> LinkedIn </Nav.Link>
+            <Nav.Link href="https://github.com/beachbrunet" target="_blank">
+              {" "}
+              Github{" "}
+            </Nav.Link>
+            <Nav.Link
+              href="https://www.linkedin.com/in/catherine-cox-433061128/"
+              target="_blank"
+            >
+              {" "}
+              LinkedIn{" "}
+            </Nav.Link>
             <Nav.Link href="#Documents"> Documents {" </> "} </Nav.Link>
           </Nav>
         </Container>
