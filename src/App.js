d4ec2c91f8f9d6e95a99248a8580d8bb7654798b
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2023-02-06 17:25:27 -0700

    added projects page ipsum

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Docs from "./components/Docs";
 // import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
+import Projects from "./components/Projects";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // bootstrap?
@@ -21,6 +22,9 @@ function App() {
           </Route>
           <Route exact path="/Docs">
             <Docs />
+          </Route>
+          <Route exact path="/Projects">
+            <Projects />
           </Route>
           <Route exact path="/Contact">
             <Contact />
---
+++ src/components/Projects/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const docsPage = () => {
+  return (
+    <div class="container">
+      <h2 className="top-title">Resume</h2>
+      <ul>
+        <h6>
+          Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro.
+          De carne lumbering animata corpora quaeritis. Summus brains sit​​,
+          morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum
+          mauris. Hi mindless mortuis soulless creaturas, imo evil stalking
+          monstra adventus resi dentevil vultus comedat cerebella viventium.
+        </h6>
+      </ul>
+    </div>
+  );
+};
+export default docsPage;
